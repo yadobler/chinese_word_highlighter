3db7b88051de387ef9ce990768aa2b5a8fc3b6ae
yukna <49450747+yadobler@users.noreply.github.com>
2025-04-09 03:57:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         vue(),
         vueDevTools(),
     ],
-    base: '/chinese_word_highligher/',
+    base: '/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
