8796ca2d539cd1b92ee232e11afb67a7f2623e41
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-04-24 02:46:06 +0800

    Add pinyin tone numbers and break file up

---
+++ db/add_tone_numbers.py
@@ -0,0 +1,29 @@
+import csv
+
+PinyinToneMark = {
+    5: "aoeiuv\u00fc",
+    1: "\u0101\u014d\u0113\u012b\u016b\u01d6\u01d6",
+    2: "\u00e1\u00f3\u00e9\u00ed\u00fa\u01d8\u01d8",
+    3: "\u01ce\u01d2\u011b\u01d0\u01d4\u01da\u01da",
+    4: "\u00e0\u00f2\u00e8\u00ec\u00f9\u01dc\u01dc",
+}
+
+results = []
+with open("./default_vaules_pre.csv") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        pinyins = []
+        for pinyin in row["Pinyin"].split(" "):
+            for i in range(1,6):
+                test = sum([1 if c in PinyinToneMark[i] else 1 for c in pinyin])
+                if test > 0:
+                    pinyin += str(i)
+                    pinyins.append(pinyin)
+                    break
+        row["Pinyin"] = " ".join(pinyins)
+        results.append(row)
+
+results.sort(key=lambda x: int(x["Chapter"]) if x["Chapter"] else 0)
+with open("../public/default_vaules.csv", "w") as f:
+    writer = csv.DictWriter(f, ["Simplified","Chapter","Pinyin","Category","Meaning"])
+    writer.writerows(results)
